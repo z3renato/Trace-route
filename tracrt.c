11668763f74fdd7390708d42806db765e4f89e0f
José Renato <zerenato.souza@bcc.unifal-mg.edu.br>
2015-07-08 18:20:04 -0300

    Ping infinito implementado. Resolução de DNS

--- tracrt.c
+++ tracrt.c
@@ -9,7 +9,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/ip_icmp.h>
-
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
@@ -17,10 +17,37 @@
 #include <netdb.h>
 #include <unistd.h>
 
+int executa = 1;
 
 typedef unsigned short int u16;
 
 unsigned short in_cksum(unsigned short *ptr, int nbytes);
+void  INThandler(int sig){
+     executa=0;
+}
+int hostname_to_ip(char * hostname , char* ip){
+    struct hostent *he;
+    struct in_addr **addr_list;
+    int i;
+         
+    if ( (he = gethostbyname( hostname ) ) == NULL) 
+    {
+       
+        herror("gethostbyname");
+        return 1;
+    }
+ 
+    addr_list = (struct in_addr **) he->h_addr_list;
+     
+    for(i = 0; addr_list[i] != NULL; i++) 
+    {
+        
+        strcpy(ip , inet_ntoa(*addr_list[i]) );
+        return 0;
+    }
+     
+    return 1;
+}
 
 int trace(unsigned long saddr, unsigned long daddr, int ttl) {
     char consulta[300];
@@ -191,7 +218,7 @@ int pingReduzido(unsigned long saddr,unsigned long daddr ){
 	printf("\nDisparando dados para (%s) \n", ipDestino);
 	struct timeval inicio, final;
 	int tempo_milisegundos;
-	int pacotesRecebidos=0 ;
+	float pacotesRecebidos=0 ;
 	float mediaPerda=0, rttMedio=0;
 	for(x=0;x<4; x++){
 		gettimeofday(&inicio, NULL);
@@ -315,7 +342,151 @@ int pingReduzido(unsigned long saddr,unsigned long daddr ){
 	}
 	mediaPerda = pacotesRecebidos/4;
 	printf("\n");
-	printf("\n%d pacotes enviados %d pacotes recebidos %2.2f tempo =%3.0f media de rtt: %4.2f TTL: %d\n ", x, pacotesRecebidos, mediaPerda*100, rttMedio, rttMedio/4, ttl);
+	printf("\n%d pacotes enviados %3.0f pacotes recebidos %2.2f %% tempo =%3.0f media de rtt: %4.2f TTL: %d\n ", x, pacotesRecebidos, (float)(pacotesRecebidos/x)*100, rttMedio, rttMedio/4, ttl);
+	return (0);
+
+
+}
+
+int pingInfinito(unsigned long saddr,unsigned long daddr ){
+	int x=0;
+	int atingido = 0;
+	int ttl=20;
+	printf("\n");
+	system("clear");
+    char ipDestino[INET_ADDRSTRLEN]; //para escrever o IP destino na tela
+    inet_ntop(AF_INET, &(daddr), ipDestino, INET_ADDRSTRLEN); //atribui o ip destino a variável "ip"
+	printf("\nDisparando dados para (%s) \n", ipDestino);
+	struct timeval inicio, final;
+	int tempo_milisegundos;
+	int pacotesRecebidos=0 ;
+	float mediaPerda=0, rttMedio=0;
+	signal(SIGINT, INThandler);
+	while(executa){
+		gettimeofday(&inicio, NULL);
+		char* buffer;//buffer que sera usado na recepcao dos dados
+		int addrlen;//tamanho da struct que contem o endereco de destino
+
+		//criacao do socket
+		int sockfd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+		if (sockfd < 0)
+		{
+			perror("Erro ao criar o socket");
+			exit(0);
+		}
+
+		int on = 1;
+
+		//definindo que o cabecalho
+		//ip sera especificado aqui
+		//no codigo
+		if (setsockopt (sockfd, IPPROTO_IP, IP_HDRINCL, (const char*)&on, sizeof (on)) == -1)
+		{
+			perror("Erro ao definir opcao do socket");
+			exit(0);
+		}
+
+		//Tamanho total do pacote formado pelo
+		//tamanho dos cabecalhos IP + ICMP
+		int packet_size = sizeof (struct iphdr) + sizeof (struct icmphdr);
+		char *packet = (char *) malloc (packet_size);
+
+		if (!packet)
+		{
+			perror("Falta de espaco na memoria");
+			close(sockfd);
+			return (0);
+		}
+
+		//cabecalho ip
+		struct iphdr *ip = (struct iphdr *) packet;
+		//cabecalho icmp
+		struct icmphdr *icmp = (struct icmphdr *) (packet + sizeof (struct iphdr));
+
+		//preenchendo pacote com zero
+		memset (packet, 0, packet_size);
+
+		ip->version = 4;//versao
+		ip->ihl = 5;//tamanho do cabecalho
+		ip->tos = 0;//TypeOfService
+		ip->tot_len = htons (packet_size);//tamanho total
+		ip->id = rand ();//id, gerado aleatoriamente
+		ip->frag_off = 0;//bit de fragmentacao
+		ip->ttl = ttl;//ttl
+		ip->protocol = IPPROTO_ICMP;//protocolo que sera usado (icmp)
+		ip->saddr = saddr;//IP de origem
+		ip->daddr = daddr;//IP de destino
+		ip->check = in_cksum ((u16 *) ip, sizeof (struct iphdr));//checksum
+
+		icmp->type = ICMP_ECHO;//tipo
+		icmp->code = 0;//codigo
+		icmp->checksum = in_cksum((unsigned short *)icmp, sizeof(struct icmphdr));//checksum
+
+		//criando struct com
+		//o endereco de destino
+		//para envio pelo socket
+		struct sockaddr_in servaddr;
+		servaddr.sin_family = AF_INET;
+		servaddr.sin_addr.s_addr = daddr;
+
+		//tamanho do struct gerado
+		addrlen = sizeof(servaddr);
+
+		//envio de dados com os parametros:
+		//socket a ser usado
+		//pacote
+		//tamanho do pacote
+		//campo de opcoes
+		//endereco de destino
+		//tamanho da struct de endereco
+		if (sendto(sockfd, packet, packet_size, 0, (struct sockaddr*) &servaddr, sizeof (servaddr)) < 1)
+		{
+			perror("Falha ao enviar\n");
+		}
+
+		//alocando o buffer de recepcao dos dados
+		buffer = malloc(sizeof(struct iphdr) + sizeof(struct icmphdr));
+		//realizando a recepcao, com parametros semelhantes ao envio
+
+		fd_set socks;
+		struct timeval t;
+		FD_ZERO(&socks);
+		FD_SET(sockfd, &socks);
+		t.tv_sec = 3; //tempo segundos timeout
+		t.tv_usec = 0;
+
+		if (select(sockfd+1, &socks, NULL, NULL, &t) &&
+				recvfrom(sockfd, buffer, sizeof(struct iphdr) + sizeof(struct icmphdr), 0, (struct sockaddr *)&servaddr, &addrlen)!=-1)
+				{
+						
+						struct iphdr* ip_reply;
+						ip_reply = (struct iphdr*) buffer;
+
+						char ipAt[INET_ADDRSTRLEN];
+						inet_ntop(AF_INET, &(ip_reply->saddr), ipAt, INET_ADDRSTRLEN);
+						printf("Resposta de: %s ",  ipAt);
+						pacotesRecebidos++;
+						gettimeofday(&final, NULL);
+						tempo_milisegundos = (float) (1000 * (final.tv_sec - inicio.tv_sec) + (final.tv_usec - inicio.tv_usec) / 1000);
+						printf(" %dms ", (int) tempo_milisegundos);
+						rttMedio+=tempo_milisegundos;
+				}
+		else{
+				puts("esgotado tempo limite(timeout)");
+				ttl+=20;
+			}
+			x++;
+		free(packet);
+		free(buffer);
+		close(sockfd);
+		printf("\n");
+		
+	}
+	float pacotesEnviados = (float) x;
+	mediaPerda = (pacotesRecebidos/pacotesEnviados)*100.0;
+	printf("\n");
+	printf("\n%d pacotes enviados %d pacotes recebidos %2.2f %% tempo =%3.0f media de rtt: %4.2f TTL: %d\n ", x, pacotesRecebidos, mediaPerda, rttMedio, rttMedio/x, ttl);
 	return (0);
 
 
@@ -327,8 +498,34 @@ int main(int argc, char **argv) {
         printf("uso: %s <IP destino>\n", argv[0]);
         exit(0);
     }
-   pingReduzido(system("ifconfig | grep 'inet end' | awk '{print $3}' | grep -v '127'"), inet_addr(argv[1]));
-    //trace(system("ifconfig | grep 'inet end' | awk '{print $3}' | grep -v '127'"), inet_addr(argv[1]), 0);
+    char  hostname[20] ; char ip[12];
+    hostname_to_ip(argv[1], ip);
+
+
+    int opc;
+   do {
+    printf("\nEscolha seu comando:\n 1-Traceroute\n2-Ping infinito\n3-Ping de 4 pacotes\n0-Sair\n");
+    scanf(" %d",&opc);
+	}
+    while(opc<0 || opc>3);
+    while(opc){
+    	executa = 1;
+    	if(opc==1)
+    		trace(system("ifconfig | grep 'inet end' | awk '{print $3}' | grep -v '127'"), inet_addr(ip), 0);
+    	if(opc==2){
+    		pingInfinito(system("ifconfig | grep 'inet end' | awk '{print $3}' | grep -v '127'"), inet_addr(ip));
+    	}
+    	if(opc==3)
+    		pingReduzido(system("ifconfig | grep 'inet end' | awk '{print $3}' | grep -v '127'"), inet_addr(ip));
+    	do {
+    		printf("\nEscolha seu comando:\n 1-Traceroute\n2-Ping infinito\n3-Ping de 4 pacotes\n0-Sair\n");
+    		scanf(" %d",&opc);
+    	}
+    	while(opc<0 || opc>3);
+    }
+
+   
+    //
     printf("\n");
 }
 
